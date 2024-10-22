5b2b67a601f346e7f44c06683223c3a30eb06357
Loff3 <olof.z1337@gmail.com>
2024-10-22 12:40:31 +0200

    UI

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,10 +1,18 @@
 package gui;
 
+import com.formdev.flatlaf.FlatDarculaLaf;
+
 import javax.swing.*;
 
 
 public class Main {
     public static void main(String[] args) {
+
+        try {
+            UIManager.setLookAndFeel( new FlatDarculaLaf() );
+        } catch( Exception ex ) {
+            System.err.println( "Failed to initialize LaF" );
+        }
         //detta är viktigt för trådsäkerhet
         SwingUtilities.invokeLater(() -> new Window());
     }
